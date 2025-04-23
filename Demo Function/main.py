dd211cf1e117e3f3f5f3dea359215a1e40ca34b2
Oliverr11 <mrrsaki0403@gmail.com>
2025-04-23 09:16:08 +0700

    Function

---
+++ Demo Function/main.py
@@ -0,0 +1,20 @@
+#function
+def greet():
+    print("Hello bro~")
+
+greet()
+
+def message(msg):
+    print(msg)
+    
+message("This is Python Basic!")
+
+def welcome():
+    return ("Welcome To Python Class!")
+
+print(welcome())
+
+def info(name,age):
+    return (f"Name : {name}, Age : {age} Years Old!")
+
+print(info("Sengkim","18"))
