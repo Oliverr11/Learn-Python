fb7be32d55b64dfccc42a11270c515e2f0d70bc6
Oliverr11 <mrrsaki0403@gmail.com>
2025-04-28 22:51:54 +0700

    Done LBM

--- mini-project/LibraryManagement/library.py
+++ mini-project/LibraryManagement/library.py
@@ -1,15 +1,22 @@
+import os
 libraries = { 
     'Programming': [
         {'bookId': '10', 'bookName': 'C++', 'isBorrowed': False},
-        {'bookId': '11', 'bookName': 'Python', 'isBorrowed': True}
+        {'bookId': '11', 'bookName': 'Python', 'isBorrowed': True},
+        {'bookId': '12', 'bookName': 'The Journey', 'isBorrowed': True}
     ],
     'Math': [
-        {'bookId': '12', 'bookName': 'Math For 1st Grade', 'isBorrowed': False}
+        {'bookId': '14', 'bookName': 'Math For 1st Grade', 'isBorrowed': False},
+        {'bookId': '18', 'bookName': 'The Journey', 'isBorrowed': True} 
+    ],
+    'Khmer':[
+        {'bookId': '10', 'bookName': 'Khmer Book For 1st Grade', 'isBorrowed': False},
     ]
 }
 def addBook():
+    print("==>> Add Book")
     displayCategory()
-    category = input("Enter Category : ")
+    category = input("Enter Category Name : ")
     searchCate = searchCategory(category)
     if searchCate != None : 
         bookName = input("Enter Book Name : ")
@@ -23,37 +30,142 @@ def addBook():
     else : 
         print("Category Not Found!")
 def addCategory():
-    category = input("Enter Category Name : ")
-    libraries[category] = []
+    print("=========== Add Category ===========")
+
+    categoryName = input("Enter Category Name : ")
+    searchCate = searchCategory(categoryName)
+    if searchCate == None :
+        libraries[categoryName] = []
+        print(f"Category {categoryName} Add Successfully!")
+        return
+    print("This Category Already Exist!")
 
 def searchCategory(categoryName):
     for key,value in libraries.items():
-        if key == categoryName:
+        if key.lower() == categoryName.lower():
             return value        
     return None
+def searchBook(categoryName,bookName):
+    searchCate = searchCategory(categoryName)
+    if searchCate != None:
+        for book in searchCate:
+            if book['bookName'].lower() == bookName.lower():
+                return book
+    return None
+def searchBooks(bookName): 
+    print(f"==>> Book Name : {bookName} ")
+    for keys,values in libraries.items():
+        for value in values:
+            if bookName.lower() == value['bookName'].lower() :
+                print(f"Category : {keys} \n ID : {value['bookId']} \n Book Name : {value['bookName']} \n Borrow : {'Availible' if value['isBorrowed'] == False else 'Not Availible' }")
+    
+def menuSearchBook():
+    print("=========== Search Book ===========")
+    bookName = input("Enter Book Name : ")
+    searchBooks(bookName)
 
+    
 def displayCategory():
-    print("===Categories===")
+    print("============== Categories ==============")
     index = 0
     for key,value in libraries.items():
         index+=1
         print(f"{index} : {key}")
 
 def displayBook():
-    print("===Display Books===")
+    print("========= Display Books =========")
     print("1) Display All Books")
     print("2) Display By Category")
     option = int(input("Enter Option (1-2) : "))
     match option :
         case 1:
+            os.system('cls')
             displayAllBooks()
         case 2:
+            os.system('cls')
             category = input("Enter Category Name : ")
             displayBooksByCategory(category)
         case _:
             print("Invalid Option!")
-
+            
+def borrowBook():
+    print("==>> Borrow Book")
+    displayCategory()
+    categoryName = input("Enter Category : ")
+    searchCate = searchCategory(categoryName)
+    if searchCate != None:
+        displayBooksByCategory(categoryName)
+        bookName = input("Enter Book Name : ")
+        sBook = searchBook(categoryName,bookName)
+        if sBook != None : 
+            if sBook['isBorrowed'] == False: 
+                sBook['isBorrowed'] = True
+                print("Borrow Successfully!")
+                return
+            else:
+                print(f"{sBook['bookName']} Is Unavailible!")
+                return
+        else:
+            print("Book Not Found!")
+            return
+    print("Category Not Found!")
+    
+def returnBook():
+    print("==>> Return Book")
+    displayCategory()
+    categoryName = input("Enter Category : ")
+    searchCate = searchCategory(categoryName)
+    if searchCate != None:
+        displayBooksByCategory(categoryName)
+        bookName = input("Enter Book Name : ")
+        sBook = searchBook(categoryName,bookName)
+        if sBook != None : 
+            if sBook['isBorrowed'] == True: 
+                sBook['isBorrowed'] = False
+                print("Return Successfully!")
+                return
+            else:
+                print(f"{sBook['bookName']} Is Availible!")
+                return
+        else:
+            print("Book Not Found!")
+            return
+            
+    print("Category Not Found!")
+    
+def returnOrBorrowBook(checkRB):
+    print("==>> Return Book")
+    displayCategory()
+    categoryName = input("Enter Category : ")
+    searchCate = searchCategory(categoryName)
+    if searchCate != None:
+        displayBooksByCategory(categoryName)
+        bookName = input("Enter Book Name : ")
+        sBook = searchBook(categoryName,bookName)
+        if sBook != None : 
+            if checkRB=='return':
+                if  sBook['isBorrowed'] == True: 
+                    sBook['isBorrowed'] = False
+                    print("Return Successfully!")
+                    return
+                else:
+                    print(f"{sBook['bookName']} Is Availible!")
+                    return
+            if checkRB=='borrow':
+                if sBook['isBorrowed'] == False: 
+                    sBook['isBorrowed'] = True
+                    print("Borrow Successfully!")
+                    return
+                else:
+                    print(f"{sBook['bookName']} Is Unavailible!")
+                    return
+        else:
+            print("Book Not Found!")
+            return
+    print("Category Not Found!")
+    
 def displayAllBooks():
+    print("=============== All Books ===============")
     for key,values in libraries.items():
         print(f"=> {key} : ")
         for value in values:
@@ -71,6 +183,7 @@ def displayBooksByCategory(categoryName):
     else : print("Category Not Found!")
     
 def menu():
+    os.system('cls')
     while True:
         print("===Library Management System ===")
         print("1) Add Book")
@@ -79,19 +192,44 @@ def menu():
         print("4) Return Book")
         print("5) Display Books")
         print("6) Search Book")
-        print("10) Print Lists")
+        #print("10) Print List [Test]")
         print("7) Exit")
         option = int(input("Enter Option (1-7) : "))
         match option:
-            case 1:
+            case 1:    
+                os.system('cls')
+                #os.system('clear') #mac
                 addBook()
             case 2:
+                os.system('cls')
+                #os.system('clear') #mac
                 addCategory()
+            case 3:
+                os.system('cls')
+                #os.system('clear') #mac
+                #borrowBook()
+                returnOrBorrowBook("borrow")
+            case 4:
+                os.system('cls')
+                #os.system('clear') #mac
+                #returnBook()
+                returnOrBorrowBook("return")
             case 5:
+                os.system('cls')
+                #os.system('clear') #mac
                 displayBook()
-            case 10:
-                print(libraries)
+            case 6:
+                os.system('cls')
+                #os.system('clear') #mac
+                menuSearchBook()
+            #case 10:
+                #print(libraries)
+            case 7:
+                print("-"*50)
+                print("Thank you for using the Library Management System!")
+                print("-"*50)
+                
+                break
             case _:
                 print("Invalid Choice!")
-                
 menu()
